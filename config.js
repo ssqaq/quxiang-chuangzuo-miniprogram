ec96db4cf4cf2c0990d4e845b7838318c62874e6
ssqaq <GitHub>
2026-08-23 22:31:28 +0800

    自动同步 2026-08-23 22:31

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ module.exports = {
     quality: 82,
     minBytes: 262144
   },
-  appVersion: "0.15.7",
+  appVersion: "0.15.8",
   photoToVideo: {
     durationSeconds: 3,
     resolution: "720p",

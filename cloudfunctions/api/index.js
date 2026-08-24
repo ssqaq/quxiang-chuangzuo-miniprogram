04ad6a721357fb060df50175d3b4dc5a6d28c7f7
ssqaq <GitHub>
2026-08-24 18:32:32 +0800

    发布照片转实况版本 0.34.0

--- cloudfunctions/api/index.js
+++ cloudfunctions/api/index.js
@@ -1,5 +1,5 @@
-const API_BUILD_VERSION = "0.32.4";
-const API_BUILD_MARKER = "API_BUILD_TAG_20260824_ADMIN_USER_FILTER_DETAIL_V324";
+const API_BUILD_VERSION = "0.34.0";
+const API_BUILD_MARKER = "API_BUILD_TAG_20260824_MOTION_PHOTO_LIVP_V340";
 console.log(`[api] build=${API_BUILD_VERSION} marker=${API_BUILD_MARKER}`);
 
 const cloud = require("wx-server-sdk");

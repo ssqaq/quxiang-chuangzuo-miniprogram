1a4309a600387e769e88a9ae75bf7371e5720207
ssqaq <GitHub>
2026-08-25 11:36:57 +0800

    自动同步：7 个文件（页面 1、云函数 3、配置 3）
    变更：修改 7
    文件：cloudfunctions/api/index.js；cloudfunctions/api/package-lock.json；cloudfunctions/api/package.json；config.js；media-worker/package-lock.json；media-worker/package.json；等 7 个文件
    版本：0.36.12
    提交前暂存 tree：f536ca00164a9e89f48c7a2c0485953c6af598b4
    发布包：D:\aips小程序\wechat-miniapp-release-v0.36.12.zip
    同步时间：2026-08-25 11:36:57

--- cloudfunctions/api/index.js
+++ cloudfunctions/api/index.js
@@ -1,5 +1,5 @@
-const API_BUILD_VERSION = "0.36.8";
-const API_BUILD_MARKER = "API_BUILD_TAG_20260825_RELEASE_SAFETY_V3608";
+const API_BUILD_VERSION = "0.36.12";
+const API_BUILD_MARKER = "API_BUILD_TAG_20260825_ADMIN_MONITOR_ORDER_V3612";
 console.log(`[api] build=${API_BUILD_VERSION} marker=${API_BUILD_MARKER}`);
 
 const cloud = require("wx-server-sdk");
